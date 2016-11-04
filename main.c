2b04e6b331235d9042ad6b331fa1827cfc55c86e
Alrick Eymauzy <alrick.eymauzy@gmail.com>
2016-11-04 00:01:12 +0000

    ajout des ex08 et ex09

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void    ft_putchar(char c)
 {
@@ -7,11 +8,21 @@ void    ft_putchar(char c)
 
 void    ft_print_alphabet(void);
 void    ft_print_numbers(void);
-
+void    ft_is_negative(int);
+void    ft_ft(int *nbr);
 
 int main()
 {
+    int nb = 6;
+    int *nbr;
+    nbr = &nb;
+    
     ft_print_alphabet();
     ft_print_numbers();
+    ft_is_negative(-1);
+    ft_is_negative(0);
+    ft_is_negative(1);
+    ft_ft(nbr);
+    printf("%d", nb);
     return 0;
 }
